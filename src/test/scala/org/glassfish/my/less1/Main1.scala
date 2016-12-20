1284b1a196b3ed246d47ec9adb45643fddb9ff9a
Sasha <skmets@gmail.com>
2016-12-20 09:59:54 +0200

    master

--- src/test/scala/org/glassfish/my/less1/Main1.scala
+++ src/test/scala/org/glassfish/my/less1/Main1.scala
@@ -15,10 +15,26 @@ import Array._
  *      http://www.w3ii.com/ru/scala/scala_variables.html
  *      http://www.w3ii.com/ru/scala/scala_data_types.html
  *      http://www.w3ii.com/ru/scala/scala_basic_syntax.html
- *      http://www.w3ii.com/ru/scala/scala_environment_setup.html
+ *      (Настройка Scala Environment) http://www.w3ii.com/ru/scala/scala_environment_setup.html
  *
- *      https://github.com/anton-k/ru-neophyte-guide-to-scala
- *      https://ru.wikibooks.org/wiki/Scala_в_примерах
+ * @see https://github.com/anton-k/ru-neophyte-guide-to-scala
+ *
+ * Scala в примерах
+ * ****************
+ * @see https://ru.wikibooks.org/wiki/Scala_в_примерах
+ *
+ * Java 8/Scala — різниця в підходах та взаємні інновації
+ * *******************************************************
+ * @see https://dou.ua/lenta/columns/java8-vs-scala/
+ *
+ * Почему Скала
+ * ************
+ * @see http://fprog.ru/2010/issue6/vlad-patryshev-why-scala/
+ *
+ * Scala Школа!
+ * ************
+ * @see https://twitter.github.io/scala_school/ru/
+ *      https://twitter.github.io/scala_school/ru/basics2.html
  */
 
 object Main1 {
@@ -28,7 +44,7 @@ object Main1 {
     a = "bar"
 
     val b: String = "foo" // константная (finul) переменная
-//    b = "bar"
+    //    b = "bar"
 
     var myList1 = Array(1.9, 2.9, 3.4, 3.5)
     var myList2 = Array(8.9, 7.9, 0.4, 1.5)
